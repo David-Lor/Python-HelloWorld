37ddce40fd39e2a923df306ce6be1da62d3ee6d9
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2022-06-17 23:39:40 +0200

    feat: sleep "forever" with signal.pause

--- python_helloworld/runner.py
+++ python_helloworld/runner.py
@@ -1,6 +1,7 @@
 """RUNNER
 """
 
+import signal
 from .helpers import get_greet
 
 __all__ = ("run",)
@@ -8,3 +9,7 @@ __all__ = ("run",)
 
 def run():
     print(get_greet())
+    print("Sleeping forever...")
+    signal.signal(signal.SIGINT, lambda *_: print("Signal: SIGINT"))
+    signal.signal(signal.SIGTERM, lambda *_: print("Signal: SIGTERM"))
+    signal.pause()
