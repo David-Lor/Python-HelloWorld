1ec3e76cdc26327166154788c759e27afdf8d1a3
David Lorenzo <17401854+David-Lor@users.noreply.github.com>
2022-06-17 23:43:40 +0200

    feat: get current PID and print at the end

--- python_helloworld/runner.py
+++ python_helloworld/runner.py
@@ -1,6 +1,8 @@
 """RUNNER
 """
 
+import os
+
 from .helpers import get_greet
 
 __all__ = ("run",)
@@ -8,3 +10,4 @@ __all__ = ("run",)
 
 def run():
     print(get_greet())
+    print(f"PID={os.getpid()}")
